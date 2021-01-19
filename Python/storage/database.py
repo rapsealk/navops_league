289e0b67f7ba3363d9e85ec4eac995b2a906e2d1
rapsealk <piono623@naver.com>
2021-01-20 04:04:34 +0900

    WIP-014: [Python] MongoDB 성능 테스트

--- Python/storage/database.py
+++ Python/storage/database.py
@@ -1,14 +1,85 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
-import sqlalchemy as db
+import os
+import json
+import pprint
+from datetime import datetime
+
+# import pandas as pd
+from pymongo import MongoClient
+# import sqlalchemy as db
+
+with open(os.path.join(os.path.dirname(__file__), 'config.json')) as f:
+    config = ''.join(f.readlines())
+    config = json.loads(config)
+    MONGO_ROOT_USERNAME = config["mongo"]["username"]
+    MONGO_ROOT_PASSWORD = config["mongo"]["password"]
 
 
+class MongoWrapper:
+    pass
+
+
+"""
+def Dictable(cls):
+    def decorated():
+        cls.to_dict = lambda: cls.__dict__.copy()
+        return cls
+    return decorated
+"""
+
+
+"""
 def main():
     engine = db.create_engine('mysql+pymysql://root:1111@localhost/rimpac')
     connection = engine.connect()
     metadata = db.MetaData()
     table = db.Table('test', metadata, autoload=True, autoload_with=engine)
     print('table:', table.columns.keys())
+"""
+
+
+def random_sample(collection, n=3):
+    # { $sample: { size: <positive integer> } }
+    pipeline = [
+        {"$sample": {"size": n}}
+    ]
+    return collection.aggregate(pipeline)
+
+
+def main():
+    mongo = MongoClient("mongodb://{}:{}@localhost:27017/".format(MONGO_ROOT_USERNAME, MONGO_ROOT_PASSWORD))
+    print(mongo.list_database_names())
+
+    database = mongo["rimpac"]
+    collection = database["transition"]
+
+    x = collection.insert_one({
+        "timestamp": datetime.now(),
+        "position": {
+            "x": 0.0,
+            "y": 0.0,
+            "z": 0.0
+        },
+        "rotation": {
+            "x": 0.0,
+            "y": 0.0,
+            "z": 0.0
+        }
+    })
+    print(x.inserted_id)
+
+    """
+    x = collections.insert_many([
+        { ... }
+    ])
+    print(x.inserted_ids)
+    """
+
+    sample = random_sample(collection)
+    # print('sample:', list(sample))
+    # print('sample:', )
+    pprint.pprint(list(sample))
 
 
 if __name__ == "__main__":
---
+++ Python/storage/database_test.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+import unittest
+import os
+import json
+from time import perf_counter
+from datetime import datetime
+
+import pymongo
+
+with open(os.path.join(os.path.dirname(__file__), 'config.json')) as f:
+    config = ''.join(f.readlines())
+    config = json.loads(config)
+    MONGO_ROOT_USERNAME = config["mongo"]["username"]
+    MONGO_ROOT_PASSWORD = config["mongo"]["password"]
+
+
+class TestDatabase(unittest.TestCase):
+
+    def setUp(self):
+        self.database = pymongo.MongoClient("mongodb://{}:{}@localhost:27017/".format(MONGO_ROOT_USERNAME, MONGO_ROOT_PASSWORD))
+        self.collection = self.database["rimpac"]["transition"]
+
+        self.collection.drop()
+        documents = []
+        for _ in range(1_000_000):    # 240mb
+            documents.append({
+                "timestamp": datetime.now(),
+                "position": {"x": 0.0, "y": 0.0, "z": 0.0},
+                "rotation": {"x": 0.0, "y": 0.0, "z": 0.0}
+            })  # 240 Bytes
+        self.collection.insert_many(documents)
+
+    def test_random_sample_performance(self):
+        size = 2048
+        counter = perf_counter()
+        pipeline = [
+            {"$sample": {"size": size}}
+        ]
+        samples = self.collection.aggregate(pipeline)
+        performance = perf_counter() - counter
+        print(performance)
+        self.assertEqual(len(list(samples)), size)
+        self.assertLess(performance, 1.0)
+
+    def tearDown(self):
+        self.collection.drop()
+
+
+if __name__ == "__main__":
+    unittest.main()
+    # suite = unittest.TestLoader().loadTestsFromModule(TestDatabase)
+    # unittest.TextTestRunner(verbosity=2).run(suite)
