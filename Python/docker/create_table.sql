f462100cd9e4b1f0eff7fa045f11c2c0083ab2c6
rapsealk <piono623@naver.com>
2021-02-12 14:40:26 +0900

    WIP-032: Docker compose (mariadb) 작성 (#17)

--- Python/docker/create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE rimpac.leaguer (
-	id INT UNSIGNED auto_increment NOT NULL PRIMARY KEY,
-	rating INT UNSIGNED DEFAULT 1200 NOT NULL
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci
-COMMENT='League participants.';
--- Python/docker/mariadb.bat
+++
@@ -1,2 +0,0 @@
-::docker pull mariadb
-docker container run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=1111 -v D:\Docker\mariadb:/var/lib/mysql --name mariadb mariadb
