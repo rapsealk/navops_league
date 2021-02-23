5528e695b7bd6871aa2403e0599c05dbf1c36f3b
rapsealk <piono623@naver.com>
2021-02-23 18:16:12 +0900

    FIX: 보상 체계(Reward shaping) 함수화

--- Python/main_dqn.py
+++ Python/main_dqn.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import argparse
 import os
 import json
 from datetime import datetime
@@ -14,12 +15,37 @@ from torch.utils.tensorboard import SummaryWriter
 
 from models.pytorch_impl import DQNAgent, DDQNAgent
 from memory import ReplayBuffer
-from utils import epsilon, SlackNotification
+from utils import SlackNotification
 from rating import EloRating
 
 with open(os.path.join(os.path.dirname(__file__), 'config.json')) as f:
     config = json.loads(''.join(f.readlines()))
     SLACK_API_TOKEN = config["slack"]["token"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', type=str, default='RimpacDiscrete-v0')
+parser.add_argument('--batch_size', type=int, default=2048)
+parser.add_argument('--seq_len', type=int, default=64)
+parser.add_argument('--aggressive_factor', type=float, default=1.0)
+parser.add_argument('--defensive_factor', type=float, default=0.7)
+args = parser.parse_args()
+
+
+ENVIRONMENT = args.env
+# Hyperparameters
+BATCH_SIZE = args.batch_size
+SEQUENCE = args.seq_len
+AGGRESSIVE_FACTOR = args.aggressive_factor
+DEFENSIVE_FACTOR = args.defensive_factor
+
+EPSILON_INITIAL_VALUE = 1.0
+EPSILON_DISCOUNT = 0.99
+EPSILON_MINIMUM = 0.01
+
+REWARD_FIELD = 2
+HITPOINT_FIELD = -1
+AMMO_FIELD = -4
+FUEL_FIELD = -3
 
 
 def discount_rewards(rewards: np.ndarray, gamma=0.998):  # 347 -> 0.5
@@ -31,17 +57,28 @@ def discount_rewards(rewards: np.ndarray, gamma=0.998):  # 347 -> 0.5
     return discounted
 
 
-ENVIRONMENT = 'RimpacDiscrete-v0'
-# Hyperparameters
-BATCH_SIZE = 2048
-SEQUENCE = 64
-AGGRESSIVE_FACTOR = 1.0
-DEFENSIVE_FACTOR = 0.7
+def get_distance(a: np.ndarray, b: np.ndarray):
+    return np.sqrt(np.sum(np.power(a - b, 2)))
 
-REWARD_FIELD = 2
-HITPOINT_FIELD = -1
-AMMO_FIELD = -4
-FUEL_FIELD = -3
+
+def get_distance_reward(a: np.ndarray, b: np.ndarray):
+    return 1 / (np.power(1 + get_distance(a, b), 2) * 100)
+
+
+def shape_rewards(observation, next_observation):
+    # Reward shaping
+    # - Blade & Soul (https://arxiv.org/abs/1904.03821)
+    hp_change = [
+        next_observation[0][HITPOINT_FIELD] - observation[0][HITPOINT_FIELD],
+        next_observation[1][HITPOINT_FIELD] - observation[1][HITPOINT_FIELD]
+    ]
+    rewards = np.array([
+        (DEFENSIVE_FACTOR * hp_change[0] - AGGRESSIVE_FACTOR * hp_change[1]),
+        (DEFENSIVE_FACTOR * hp_change[1] - AGGRESSIVE_FACTOR * hp_change[0])
+    ]) + get_distance_reward(next_observation[0][:2], next_observation[1][:2])
+    # reward[0] = hp_change[0] - hp_change[1] + 10 * (info.get('win', -1) == 0)
+    # reward[1] = hp_change[1] - hp_change[0] + 10 * (info.get('win', -1) == 1)
+    return rewards
 
 
 class Learner:
@@ -77,17 +114,18 @@ class Learner:
             worker.join()
 
     def validate(self):
-        n = self._env.observation_space.shape[0]
+        observation_shape = self._env.observation_space.shape[0]
         results = []
         ratings = (1200, 1200)
         best_rating = 1200
         for episode in count(1):
+            rewards = []
             new_obs1, new_obs2 = self._env.reset()
 
-            obs1 = np.zeros((n * SEQUENCE,))
-            obs1 = np.concatenate((obs1[n:], new_obs1))
-            obs2 = np.zeros((n * SEQUENCE,))
-            obs2 = np.concatenate((obs1[n:], new_obs2))
+            obs1 = np.zeros((observation_shape * SEQUENCE,))
+            obs1 = np.concatenate((obs1[observation_shape:], new_obs1))
+            obs2 = np.zeros((observation_shape * SEQUENCE,))
+            obs2 = np.concatenate((obs1[observation_shape:], new_obs2))
 
             for timestep in count(1):
                 action1 = self._opponent_agent.get_action(obs1)
@@ -96,8 +134,11 @@ class Learner:
 
                 next_obs, reward, done, info = self._env.step(action)
 
-                next_obs1 = np.concatenate((obs1[n:], next_obs[0]))
-                next_obs2 = np.concatenate((obs2[n:], next_obs[1]))
+                _, reward = shape_rewards((obs1, obs2), next_obs)
+                rewards.append(reward)
+
+                next_obs1 = np.concatenate((obs1[observation_shape:], next_obs[0]))
+                next_obs2 = np.concatenate((obs2[observation_shape:], next_obs[1]))
 
                 obs1, obs2 = next_obs1, next_obs2
 
@@ -110,6 +151,9 @@ class Learner:
                         results.append(not a_win)
                         ratings = EloRating.calc(ratings[0], ratings[1], a_win)
                     self._writer.add_scalar('r/rating', ratings[1], episode)
+
+                    rewards = discount_rewards(rewards)
+                    self._writer.add_scalar('r/rewards', np.mean(rewards), episode)
 
                     if ratings[1] > best_rating:
                         self._target_agent.save(
@@ -182,7 +226,7 @@ class Worker(Thread):
 
     def run(self):
         observation_shape = self._env.observation_space.shape[0]
-        eps = epsilon()
+        epsilon = EPSILON_INITIAL_VALUE
         for episode in count(1):
             new_obs1, new_obs2 = self._env.reset()
 
@@ -198,31 +242,17 @@ class Worker(Thread):
             buffer1, buffer2 = [], []
 
             while True:
-                if next(eps) < np.random.random():
-                    action1 = self._agent1.get_action(obs1)
-                    action2 = self._agent2.get_action(obs2)
-                else:
+                if epsilon > np.random.random():
                     action1 = np.random.randint(self._env.action_space.n)
                     action2 = np.random.randint(self._env.action_space.n)
+                else:
+                    action1 = self._agent1.get_action(obs1)
+                    action2 = self._agent2.get_action(obs2)
                 action = np.array([action1, action2], dtype=np.uint8)
 
                 next_obs, reward, done, info = self._env.step(action)
 
-                # Reward shaping
-                # - Blade & Soul (https://arxiv.org/abs/1904.03821)
-                hp_change = [
-                    next_obs[0][HITPOINT_FIELD] - obs1[HITPOINT_FIELD],
-                    next_obs[1][HITPOINT_FIELD] - obs2[HITPOINT_FIELD]
-                ]
-                reward[0] = (DEFENSIVE_FACTOR * hp_change[0] - AGGRESSIVE_FACTOR * hp_change[1]) \
-                            * np.mean((next_obs[0][FUEL_FIELD], next_obs[0][AMMO_FIELD])) \
-                            + 10 * (info.get('win', -1) == 0) - 10 * (info.get('win', -1) == 1)
-                reward[1] = (DEFENSIVE_FACTOR * hp_change[1] - AGGRESSIVE_FACTOR * hp_change[0]) \
-                            * np.mean((next_obs[1][FUEL_FIELD], next_obs[1][AMMO_FIELD])) \
-                            + 10 * (info.get('win', -1) == 1) - 10 * (info.get('win', -1) == 0)
-                # reward[0] = hp_change[0] - hp_change[1] + 10 * (info.get('win', -1) == 0)
-                # reward[1] = hp_change[1] - hp_change[0] + 10 * (info.get('win', -1) == 1)
-                # === reward shaping
+                reward = shape_rewards((obs1, obs2), next_obs)
 
                 next_obs1 = np.concatenate((obs1[observation_shape:], next_obs[0]))
                 next_obs2 = np.concatenate((obs2[observation_shape:], next_obs[1]))
@@ -236,28 +266,12 @@ class Worker(Thread):
                     trajectory1 = np.stack(buffer1)
                     trajectory2 = np.stack(buffer2)
 
-                    """
-                    winner = info.get('win', 0)
-                    if winner == -1:    # Draw
-                        hp_diff = trajectory1[-2, 3][HITPOINT_FIELD] - trajectory2[-2, 3][HITPOINT_FIELD]
-                        if hp_diff > 0:
-                            reward1 = hp_diff * np.mean((trajectory1[-2, 3][AMMO_FIELD], trajectory1[-2, 3][FUEL_FIELD]))  # Resource Loss
-                            reward2 = 0
-                        else:
-                            reward2 = -hp_diff * np.mean((trajectory2[-2, 3][AMMO_FIELD], trajectory2[-2, 3][FUEL_FIELD]))
-                            reward1 = 0
-                    else:
-                        reward1 = 1 - winner
-                        reward2 = winner
-
-                    trajectory1[:, REWARD_FIELD] = reward1
-                    trajectory2[:, REWARD_FIELD] = reward2
-                    """
-
                     trajectory1[:, REWARD_FIELD] = discount_rewards(trajectory1[:, REWARD_FIELD])
                     trajectory2[:, REWARD_FIELD] = discount_rewards(trajectory2[:, REWARD_FIELD])
                     for s, a, r, s_, d in np.concatenate([trajectory1, trajectory2]):
                         self._buffer.push(s, a, r, s_, d)
+
+                    epsilon = max(epsilon * EPSILON_DISCOUNT, EPSILON_MINIMUM)
 
                     break
 
@@ -265,84 +279,6 @@ class Worker(Thread):
 @SlackNotification(SLACK_API_TOKEN)
 def main():
     Learner().run()
-
-    """
-    env = gym.make('RimpacDiscrete-v0', mock=True)
-    n = env.observation_space.shape[0]
-
-    agent1 = DQNAgent(
-        env.observation_space.shape[0],
-        env.action_space.n,
-        BATCH_SIZE
-    )
-    agent2 = DQNAgent(
-        env.observation_space.shape[0],
-        env.action_space.n,
-        BATCH_SIZE
-    )
-    memory = ReplayBuffer()
-    buffer1 = []
-    buffer2 = []
-    results = []
-    ratings = (1200, 1200)
-    best_rating = 1200
-
-    writer = SummaryWriter(f'runs/{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}-RimpacDiscrete-v0')
-
-    eps = epsilon()
-    for episode in count(1):
-        new_obs1, new_obs2 = env.reset()
-
-        obs1 = np.zeros((n * SEQUENCE,))
-        obs1 = np.concatenate((obs1[n:], new_obs1))
-        obs2 = np.zeros((n * SEQUENCE,))
-        obs2 = np.concatenate((obs1[n:], new_obs2))
-
-        while True:
-            if next(eps) < np.random.random():
-                action1 = agent1.get_action(obs1)
-                action2 = agent2.get_action(obs2)
-            else:
-                action1 = np.random.randint(env.action_space.n)
-                action2 = np.random.randint(env.action_space.n)
-            action = np.array([action1, action2], dtype=np.uint8)
-
-            next_obs, reward, done, info = env.step(action)
-
-            next_obs1 = np.concatenate((obs1[n:], next_obs[0]))
-            next_obs2 = np.concatenate((obs2[n:], next_obs[1]))
-
-            buffer1.append((obs1, action1, reward[0], next_obs1, done))
-            buffer2.append((obs2, action2, reward[1], next_obs2, done))
-
-            obs1, obs2 = next_obs1, next_obs2
-
-            if done:
-                a_win = bool(1 - info.get('win', 0))
-                results.append(a_win)
-                ratings = EloRating.calc(ratings[0], ratings[1], a_win)
-                writer.add_scalar('r/rating1', ratings[0], episode)
-                writer.add_scalar('r/rating2', ratings[1], episode)
-
-                trajectory1 = np.stack(buffer1)
-                trajectory2 = np.stack(buffer2)
-                trajectory1[:, 2] = discount_rewards(trajectory1[:, 2])
-                trajectory2[:, 2] = discount_rewards(trajectory2[:, 2])
-                for s, a, r, s_, d in np.concatenate([trajectory1, trajectory2]):
-                    memory.push(s, a, r, s_, d)
-
-                if len(memory) >= BATCH_SIZE:
-                    loss1 = agent1.train(memory)
-                    loss2 = agent2.train(memory)
-                    writer.add_scalar('loss/q1', loss1, episode)
-                    writer.add_scalar('loss/q2', loss2, episode)
-
-                if episode % 100 == 0:
-                    writer.add_scalar('r/result', np.mean(results), episode // 100)
-                    results.clear()
-
-                break
-    """
 
 
 if __name__ == "__main__":
