c7b95b4715cfdfd1ea41467a4acf50d9a18d1829
rapsealk <piono623@naver.com>
2021-05-19 16:58:50 +0900

    FIX: RabbitMQ basic_consume

--- Python/rabbitmq_example_receive.py
+++ Python/rabbitmq_example_receive.py
@@ -14,7 +14,7 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.queue_declare(queue='hello')
-    channel.basic_comsume(queue='hello',
+    channel.basic_consume(queue='hello',
                           auto_ack=True,
                           on_message_callback=callback)
     print('[*] Waiting for messages. To exit press CTRL+C')
--- Python/rabbitmq_example_send.py
+++ Python/rabbitmq_example_send.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+import time
+import json
+from datetime import datetime
+
 import pika
 
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.35.126'))
     channel = connection.channel()
     channel.queue_declare(queue='hello')
-    channel.basic_publish(exchange='',
-                          routing_key='hello',
-                          body='Hello world!')
-    print('[x] Sent "Hello world!"')
+
+    while True:
+        message = input()
+        if message == 'q':
+            break
+
+        t = time.thread_time()
+        message = {"timestamp": time.time(), "message": message}
+        message = json.dumps(message)
+        channel.basic_publish(exchange='',
+                              routing_key='hello',
+                              body=message)
+        print(f'[{datetime.now().isoformat()}] Sent "{message}" ({time.thread_time() - t}s)')
+
     connection.close()
 
 
