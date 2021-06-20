16a27a42221ead0bad9546635210f4e639fd0dd7
rapsealk <piono623@naver.com>
2021-06-21 06:09:34 +0900

    WIP: reward plot 추가

--- Python/utils/board.py
+++ Python/utils/board.py
@@ -21,8 +21,20 @@ class ReportingBoard:
         if not os.path.exists(self.dirname):
             pathlib.Path(self.dirname).mkdir(parents=True, exist_ok=True)
 
-    def plot(self, y, x=None):
-        pass
+    def plot(self, rewards):
+        rewards_ = [rewards[0]]
+        for i in range(1, len(rewards)):
+            rewards_.append(rewards_[i-1] * 0.9 + rewards[i] * 0.1)
+        df = pd.DataFrame({
+            "Episode": range(1, len(rewards)+1),
+            "Reward": rewards_
+        })
+        seaborn.lineplot(x='Episode', y='Reward', color='#FF6F00',
+                         ci=95,
+                         #hue='AverageValue',
+                         #style='event',
+                         data=df)
+        plt.savefig(os.path.join(os.path.dirname(__file__), 'seaborn_rewards.png'))
 
     def plot_winning_rate(self, wins, draws, losses,
                           colors=['#00AB84', '#F6D258', '#F2552C']):
@@ -32,11 +44,13 @@ class ReportingBoard:
         plt.figure(figsize=(16, 9))
 
         for i in reversed(range(3)):
+            x = [i*100 for i in range(1, len(results)+1)]
             df = pd.DataFrame({
-                "Episode": range(1, len(results)+1),
-                "Rate": [r[i] for r in results]
+                "Episode": x,
+                # "Episode": range(1, len(results)+1),
+                "Win Rates (per 100 eps)": [r[i] for r in results]
             })
-            seaborn.barplot(x='Episode', y='Rate', data=df, color=colors[i])
+            seaborn.barplot(x='Episode', y='Win Rates (per 100 eps)', data=df, color=colors[i])
 
         plt.legend(
             bbox_to_anchor=(1.01, 1),
--- Python/waypoint_scatter.py
+++ Python/waypoint_scatter.py
@@ -6,7 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 
-from database import MongoDatabase
+from utils.database import MongoDatabase
+from utils.board import ReportingBoard
 
 seaborn.set_theme(style='darkgrid')
 
@@ -18,15 +19,41 @@ def main():
     results = result_db.get()
     session_id = results[-1]["session"]
 
-    for episode in range(1, len(result_db.get())):
+    episodes = [ep for ep in results if ep["session"] == session_id]
+
+    # Reward """
+    rewards = [episode["reward"] for episode in episodes]
+    board = ReportingBoard()
+    board.plot(rewards)
+    return
+    # """
+
+    w, d, l = [], [], []
+
+    results = [episode["result"] for episode in episodes]
+    for i in range(len(results) // 100 + 1):
+        w.append(np.sum(results[i*100:(i+1)*100]))
+        d.append(0)
+        l.append(100 - np.sum(results[i*100:(i+1)*100]))
+
+    print(len(w), len(d), len(l))
+
+    board = ReportingBoard()
+    board.plot_winning_rate(w, d, l)
+    return
+
+    for episode in range(1, len(results)):
+        if episode != 2004:
+            continue
         episode_id = ('0' * 10 + str(episode))[-10:]
         doc_id = session_id + '.' + episode_id
         documents = database.ref(doc_id).get()
 
         positions = np.array([document["position"] for document in documents])
         for n in range(positions.shape[1]):
-            positions_ = positions[:, n].squeeze()
-            indices_4 = list(filter(lambda x: x % 4 == 0, range(len(positions_))))
+            positions_ = positions  #[:, n].squeeze()
+            # indices_4 = list(filter(lambda x: x % 4 == 0, range(len(positions_))))
+            indices_4 = list(filter(lambda x: x % 16 == 0, range(len(positions_))))
             positions_ = np.take(positions_, indices_4, axis=0)
             df = pd.DataFrame({
                 "x": -positions_[:, 0],
@@ -40,8 +67,9 @@ def main():
 
         positions = np.array([document["opponent"]["position"] for document in documents])
         for n in range(positions.shape[1]):
-            positions_ = positions[:, n].squeeze()
-            indices_4 = list(filter(lambda x: x % 4 == 0, range(len(positions_))))
+            positions_ = positions  #[:, n].squeeze()
+            # indices_4 = list(filter(lambda x: x % 4 == 0, range(len(positions_))))
+            indices_4 = list(filter(lambda x: x % 16 == 0, range(len(positions_))))
             positions_ = np.take(positions_, indices_4, axis=0)
             df = pd.DataFrame({
                 "x": -positions_[:, 0],
@@ -53,17 +81,60 @@ def main():
             ax = seaborn.scatterplot(x='x', y='y', hue='timestamp', data=df, legend=False, palette=color_palette)
             ax.set(xlim=(-1.0, 1.0), ylim=(-1.0, 1.0))
 
+        # Obstacles
+        obstacles = np.array([
+            [-0.1, 0.2],
+            [-0.2, 0.1],
+            [-0.3, 0.2],
+            [0.1, -0.2],
+            [0.2, -0.1],
+            [0.3, -0.2],
+        ])
+        for n in range(obstacles.shape[1]):
+            df = pd.DataFrame({
+                "x": -obstacles[:, 0],
+                "y": -obstacles[:, 1],
+                "timestamp": [i+1 for i in range(len(obstacles))]
+            })
+            # colors = ["flare", "crest"]
+            color_palette = seaborn.color_palette("dark:black", as_cmap=True)
+            ax = seaborn.scatterplot(x='x', y='y', hue='timestamp', data=df, legend=False, palette=color_palette)
+            ax.set(xlim=(-1.0, 1.0), ylim=(-1.0, 1.0))
+
+
+        """
+        # Areas
+        areas = np.array([
+            [-0.2, 0.3],
+            [0.0, 0.0],
+            [0.2, -0.3]
+        ])
+        for n in range(areas.shape[1]):
+            df = pd.DataFrame({
+                "x": -areas[:, 0],
+                "y": -areas[:, 1],
+                "timestamp": [i+1 for i in range(len(areas))]
+            })
+            # colors = ["flare", "crest"]
+            color_palette = seaborn.color_palette("dark:green", as_cmap=True)
+            ax = seaborn.scatterplot(x='x', y='y', hue='timestamp', data=df, legend=False, palette=color_palette)
+            ax.set(xlim=(-1.0, 1.0), ylim=(-1.0, 1.0))
+        """
+
         # Custom legend
         custom = [Line2D([], [], marker='.', color='b', linestyle='None'),
-                  Line2D([], [], marker='.', color='r', linestyle='None')]
+                  Line2D([], [], marker='.', color='r', linestyle='None'),
+                  Line2D([], [], marker='.', color='black', linestyle='None')]
 
         # fig = plt.figure(figsize=(8,5))
 
-        plt.legend(custom, ['RL-CGF', 'RB-CGF'], loc='upper right')
+        plt.legend(custom, ['RL-CGF', 'RB-CGF', 'Obstacle'], loc='upper right')
 
         plt.savefig(f'scatter_{session_id}_{episode_id}.png')
         # plt.show()
         plt.clf()
+
+        break
 
     """
     positions = np.array([document["opponent"]["position"][0] for document in documents])
