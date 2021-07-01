0d9bc2afb5542a8fc04a971239f4bd8fc9646357
rapsealk <piono623@naver.com>
2021-07-02 01:54:30 +0900

    WIP: OpenCV를 이용하여 mp4 생성 (Windows 테스트)

---
+++ png2mp4.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os
+
+import cv2
+import numpy as np
+from PIL import Image
+
+
+def main():
+    dirname = os.path.join('C:\\', 'Users', 'rapsealk', 'Desktop', 'Screenshots')
+    files = os.listdir(dirname)
+    print(files)
+
+    # fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    # writer = cv2.VideoWriter('output.avi', fourcc, fps=10, frameSize=(792, 945))
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    writer = cv2.VideoWriter('output.mp4', fourcc, fps=10, frameSize=(792, 945))
+
+    for file_ in files:
+        image = Image.open(os.path.join(dirname, file_))
+        image = np.asarray(image)[:, :, ::-1]
+        writer.write(image)
+
+    writer.release()
+
+
+if __name__ == "__main__":
+    main()
